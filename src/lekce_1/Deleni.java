b32f89e47f61fc75048755e1ba79608258b09f25
micha <micha@192.168.0.122>
2018-12-18 17:37:55 +0100

    Úprava projektu Deleni.

--- src/lekce_1/Deleni.java
+++ src/lekce_1/Deleni.java
@@ -5,8 +5,10 @@ public class Deleni {
 	public static void main(String[] args) {
 		int var1 = 12;
 		double var2 = 3.5;
+		double var3 = 1;
 		
 		double result = var1 / var2;
+		double result1 = 12 / 1;
 				
 				System.out.println("Vysledek je: " + result);
 
